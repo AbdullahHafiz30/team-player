bcdc3f572836248fadf418a1beefd543b639fc32
AbdullahHafiz30 <ABDULLAH_MH30@outlook.com>
2025-03-05 02:10:10 -0800

    Initial Commit

---
+++ Teams-PlayersApp/Teams_PlayersAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Teams_PlayersAppApp.swift
+//  Teams-PlayersApp
+//
+//  Created by عبدالله حافظ on 05/09/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct Teams_PlayersAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
